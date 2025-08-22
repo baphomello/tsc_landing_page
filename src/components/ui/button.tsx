e8a9703b844b942f7f126b8228a517b83ad166e8
baphomello <baphomello@gmail.com>
2025-08-22 12:48:19 -0300

    fix: multiple properties with the same name

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,6 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        lg: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
       },
